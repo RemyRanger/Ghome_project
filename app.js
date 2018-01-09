81e14c01b826a5cde7d1fe1bade547823e5c1ec2
Rémy <remy.ranger@gmail.com>
2018-01-09 14:34:46 +0100

    prettyfy code

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ var app = express();
 //USE JSON FORMAT
 app.use(bodyParser.json());
 
-//GET Formulaire
+//SET DISCUSSION FLAG AT 0
+app.set('formulaire', '0');
+
+//LOAD QUEST FROM GFORMS
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
     var query = 'nothing';
     if (err) {
@@ -31,77 +34,92 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
     authorize(JSON.parse(content), callAppsScript, query);
 });
 
-app.post('/', function(req, response) {
+//START DISCUSSION
+app.post('/api', function(req, response) {
     //GET MSG FROM GHOME
     var query = req.body.result.parameters.msg;
-    var body = req.body.result.parameters;
-    console.log('From Google home:' + body);
+    console.log('From Google home:' + query);
     //SET JSON FORMAT AS ANSWER
     response.setHeader('Content-Type', 'application/json');
     //SET VAR
     var oui = 'oui';
-    var undef = 'undefined';
     var questions = app.get('questions');
-    //START DISCUSSION
-    if (query == oui && !app.get('response0')) {
-        var rep = questions[0];
-        console.log('passage');
-        sendResponse(response, rep)
-    } else if (!app.get('response0')) {
-        if (checkOuiNon(questions[0], query) == false) {
-          response.send(JSON.parse('{ "speech": "Votre réponse doit contenire un oui ou un non, veuillez reformuler.", "displayText": "Votre réponse doit contenire un oui ou un non, veuillez reformuler."}'));
-        } else {
-          app.set('response0', query);
-          console.log(app.get('response0'));
-          var rep = questions[1];
-          sendResponse(response, rep)
-        }
-    } else if (!app.get('response1')) {
-      if (checkOuiNon(questions[1], query) == false) {
-        response.send(JSON.parse('{ "speech": "Votre réponse doit contenire un oui ou un non, veuillez reformuler.", "displayText": "Votre réponse doit contenire un oui ou un non, veuillez reformuler."}'));
-      } else {
-        app.set('response1', query);
-        console.log(app.get('response1'));
-        var rep = questions[2];
-        sendResponse(response, rep)
-      }
-    } else if (!app.get('response2')) {
-      if (checkOuiNon(questions[2], query) == false) {
-        response.send(JSON.parse('{ "speech": "Votre réponse doit contenire un oui ou un non, veuillez reformuler.", "displayText": "Votre réponse doit contenire un oui ou un non, veuillez reformuler."}'));
-      } else {
-        app.set('response2', query);
-        console.log(app.get('response2'));
-        var rep = questions[3];
-        sendResponse(response, rep)
-      }
-    } else if (!app.get('response3')) {
-        app.set('response3', query);
-        console.log(app.get('response3'));
-
-        // Load client secrets from a local file.
-        fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-            if (err) {
-                console.log('Error loading client secret file: ' + err);
-                return;
-            }
-            // Authorize a client with the loaded credentials, then call the
-            // Google Apps Script Execution API.
-            var myrep = [app.get('response0'), app.get('response1'), app.get('response2'), query];
-            authorize(JSON.parse(content), postFormulaire, myrep);
-        });
-        response.send(JSON.parse('{ "speech": "Formulaire terminé", "displayText": "formualire terminé", "data": { "google": {"expect_user_response": false}}}'));
+    //IF PROCESSING FORM
+    if (app.get('formulaire')=='1') {
+      processForm(query, response)
+    } else if (~query.indexOf("remplir formulaire")) { //CASE OF DISCUSSION
+      app.set('formulaire', '1');
+      var rep = questions[0];
+      sendResponse(response, rep);
+    } else {
+      response.send(JSON.parse('{ "speech": "Je n\'ai pas compris. Pouvez vous reformuler votre demande ?", "displayText": "Je n\'ai pas compris. Pouvez vous reformuler votre demande ?"}'));
     }
 });
 
+// RUNNING SERVER
 var port = 80;
 app.listen(port);
 
+//USEFULL FUNCTIONS
+function processForm(query, response) {
+  //SET VAR
+  var questions = app.get('questions');
+
+  //START DISCUSSION
+  if (!app.get('response0')) {
+      if (checkOuiNon(questions[0], query) == false) {
+        response.send(JSON.parse('{ "speech": "Votre réponse doit contenire un oui ou un non, veuillez reformuler.", "displayText": "Votre réponse doit contenire un oui ou un non, veuillez reformuler."}'));
+      } else {
+        app.set('response0', query);
+        console.log(app.get('response0'));
+        var rep = questions[1];
+        sendResponse(response, rep);
+      }
+  } else if (!app.get('response1')) {
+    if (checkOuiNon(questions[1], query) == false) {
+      response.send(JSON.parse('{ "speech": "Votre réponse doit contenire un oui ou un non, veuillez reformuler.", "displayText": "Votre réponse doit contenire un oui ou un non, veuillez reformuler."}'));
+    } else {
+      app.set('response1', query);
+      console.log(app.get('response1'));
+      var rep = questions[2];
+      sendResponse(response, rep);
+    }
+  } else if (!app.get('response2')) {
+    if (checkOuiNon(questions[2], query) == false) {
+      response.send(JSON.parse('{ "speech": "Votre réponse doit contenire un oui ou un non, veuillez reformuler.", "displayText": "Votre réponse doit contenire un oui ou un non, veuillez reformuler."}'));
+    } else {
+      app.set('response2', query);
+      console.log(app.get('response2'));
+      var rep = questions[3];
+      sendResponse(response, rep);
+    }
+  } else if (!app.get('response3')) {
+      app.set('response3', query);
+      console.log(app.get('response3'));
+
+      // Load client secrets from a local file.
+      fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+          if (err) {
+              console.log('Error loading client secret file: ' + err);
+              return;
+          }
+          // Authorize a client with the loaded credentials, then call the
+          // Google Apps Script Execution API.
+          var myrep = [app.get('response0'), app.get('response1'), app.get('response2'), query];
+          authorize(JSON.parse(content), postFormulaire, myrep);
+      });
+      response.send(JSON.parse('{ "speech": "Formulaire terminé", "displayText": "formualire terminé", "data": { "google": {"expect_user_response": false}}}'));
+  }
+}
+
 function sendResponse(response, question) {
+    //REMOVE [F] FLAG
     if (~question.indexOf("[F]")) {
       question = question.substring(3);
       response.send(JSON.parse('{ "speech": "' + question + '", "displayText": "' + question + '"}'));
+    } else {
+      response.send(JSON.parse('{ "speech": "' + question + '", "displayText": "' + question + '"}'));
     }
-    response.send(JSON.parse('{ "speech": "' + question + '", "displayText": "' + question + '"}'));
 }
 
 function checkOuiNon(question, reponse) {
@@ -114,6 +132,8 @@ function checkOuiNon(question, reponse) {
     return true;
 }
 
+
+//GOOGLE FUNCTIONS FOR APP SCRIPT
 function authorize(credentials, callback, query) {
     var clientSecret = credentials.installed.client_secret;
     var clientId = credentials.installed.client_id;
