d9a337f836458c776344d0f9dbd7839e4c592e38
rob2b <proquotrobin@gmail.com>
2018-01-18 10:08:56 +0100

    sock v3

--- sock-test-rob.js
+++ sock-test-rob.js
@@ -5,7 +5,9 @@ const diaSock = 'wss://appartement:appartement@diasuitebox-jvm2.bordeaux.inria.f
 const clientName = 'googlehome';
 const pingInterval = 45000;
 const pingValue = 'PING:';
-let diaData = {
+var ping;
+
+var diaData = {
     type: clientName,
     data: {
         action: "send_request",
@@ -15,30 +17,35 @@ let diaData = {
     }
 };
 
-
-const ws2 = new WebSocket(diaSock, {
+var ws2 = new WebSocket(diaSock, {
     rejectUnauthorized: false
 });
 
-ws2.on('open', ()=> {
+ws2.on('open', () => {
     ws2.send("CLIENT:" + clientName, () => {
-        setInterval(()=>{ws2.send(pingValue)},pingInterval);
+        ping = setInterval(()=>{ws2.send(pingValue)},pingInterval);
+        ws2.send(JSON.stringify(diaData))
     });
 });
 
 ws2.on('message', (data) => {
-    console.log("ws2 : " + data);
+    filter(data)
 });
 
 ws2.on('close', function close() {
-    console.log('ws2 : disconnected');
-    try {
-        console.log('ws2 : trying reconnect')
-        ws2 = new WebSocket(diaSock, {
-            rejectUnauthorized: false
-        });
-    } catch (error) {
-        console.log('ws2 : socket is dead')
-    }
+    console.log('ws2 disconnected');
 });
 
+function closeSocket(ws2){
+    ws2.close();
+    console.log(ping)
+    clearInterval(ping);
+    console.log(ping)
+};
+
+function filter(data){
+    if((data.type).equals(clientName)){
+        console.log("ws2 : " + data);
+        setTimeout(()=>{closeSocket(ws2)},2500);
+    }
+}
